80996a38b9f653d2622a47c1591547fcf35bcfc5
perrywang <perrywangsh@gmail.com>
2016-03-08 23:43:05 +0800

    initial mobile check in

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,13 +12,6 @@ class HomeController extends Controller
 {
     public function show()
     {
-          $cities_url=[];
-          $cities = \App\City::select('name_en as name','id')
-              ->whereIn('name',['LasVegas','NewYork','Chicago'])
-              ->get()->all();
-          foreach($cities as $city){
-              $cities_url[$city->name] = '/tops/'.$city->id;
-          }
-          return view('home', compact('cities_url'));
+          return view('home');
     }
 }
