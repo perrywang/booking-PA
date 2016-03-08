246a809959c99057e838ac6e8ec12d30ae720740
perrywang <perrywangsh@gmail.com>
2016-03-08 23:47:45 +0800

    fix menu at bottom with a tag

--- resources/views/partial/footer_cn.blade.php
+++ resources/views/partial/footer_cn.blade.php
@@ -1,14 +1,14 @@
 <div id="footer" class="ui four item inverted red menu">
- <div class="item">
+ <a class="item">
    <img src="/images/flights.png" alt="" />
- </div>
- <div class="item">
+ </a>
+ <a class="item">
    <img src="/images/hotels.png" alt="" />
- </div>
- <div class="item">
+ </a>
+ <a class="item">
    <img src="/images/activities.png" alt="" />
- </div>
- <div class="item">
+ </a>
+ <a class="item">
    <img src="/images/flight-hotel.png" alt="" />
- </div>
+ </a>
 </div>
